700a718d5db38ae444cc3d7e7da2edefc1c65ee2
manu7772 <manu7772@gmail.com>
2014-10-08 09:51:36 +0200

    ajout navbar

--- Controller/filemakerController.php
+++ Controller/filemakerController.php
@@ -6,8 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class filemakerController extends Controller {
 
-    public function indexAction($name) {
-        return $this->render('filemakerBundle:pages:homepage.html.twig', array('name' => $name));
-    }
+	/**
+	 * page d'accueil
+	 */
+	public function indexAction($name) {
+		$data = array();
+		$data['name'] = $name;
+		return $this->render('filemakerBundle:pages:homepage.html.twig', $data);
+	}
 
+
+	/**
+	 * Affichage de la barre de navigation
+	 */
+	public function navbarAction() {
+		$data = array();
+		return $this->render('filemakerBundle:blocs:navbar.html.twig', $data);
+	}
 }
